e75a63e36033c5c33a13e3118c9a8b4d928e6a7b
Mohammad Azmi Abdussyukur <115864496+MohAzmii04@users.noreply.github.com>
2025-07-06 02:50:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ if uploaded_file is not None:
         df['Rata_Rata_Penyelesaian(%)'] = df[selesai_terakhir].mean(axis=1)
 
         def klasifikasi(p):
-            if p > 70:
+            if p > 55:
                 return 'Tinggi'
-            elif p >= 55:
+            elif p >= 70:
                 return 'Sedang'
             return 'Rendah'
 
